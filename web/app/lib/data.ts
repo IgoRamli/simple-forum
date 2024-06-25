9b483eb8cff99ac02d5c090a229a0be863072e0d
Inigo <igoramli.igo@gmail.com>
2024-06-25 23:09:13 +0800

    [Web] Implement sequential fetching to fetch posts

--- web/app/lib/data.ts
+++ web/app/lib/data.ts
@@ -1,5 +1,7 @@
 import { Session } from "next-auth";
 
+const GET_POSTS_BATCH_SIZE = 50;
+
 export interface GetPostsResponse {
   id: number,
   content: string,
@@ -10,15 +12,46 @@ export interface GetPostsResponse {
 
 export async function fetchForum(searchQuery: string, session: Session) : Promise<Array<GetPostsResponse>> {
   try {
-    const postsList = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/posts?keyword=${searchQuery}`, {
+    const postsList = [] as Array<GetPostsResponse>;
+    let lastId = undefined;
+    while (true) {
+      const batch = await fetchBatchOfPosts(session, searchQuery, GET_POSTS_BATCH_SIZE, lastId);
+      postsList.push(...batch);
+      if (batch.length < GET_POSTS_BATCH_SIZE) {
+        break;
+      }
+      lastId = batch[batch.length-1].id;
+    }
+    return postsList;
+  } catch (error) {
+    console.error(`Failed to fetch forum posts : ${error}`);
+    throw new Error(`Failed to fetch forum posts : ${error}`);
+  }
+}
+
+async function fetchBatchOfPosts(session: Session, searchQuery: string | undefined, limit: number, lastId: number | undefined) : Promise<Array<GetPostsResponse>> {
+  try {
+    const path = new URL(`${process.env.NEXT_PUBLIC_API_BASE_URL}/posts`);
+    path.searchParams.append("limit", `${limit}`);
+    if (searchQuery) {
+      path.searchParams.append("keyword", searchQuery);
+    }
+    if (lastId) {
+      path.searchParams.append("lastId", `${lastId}`);
+    }
+
+    const postsList = await fetch(path, {
       headers: {
         'Authorization': getBearerToken(session),
       }
     });
+    if (postsList.status != 200) {
+      throw new Error(`Get batch of posts return status code ${postsList.status} : ${postsList.json()}`);
+    }
     return postsList.json();
   } catch (error) {
-    console.error(`Failed to fetch forum posts : ${error}`);
-    throw new Error(`Failed to fetch forum posts : ${error}`);
+    console.error(`Failed to fetch posts batch [limit=${limit}, lastId=${lastId}] : ${error}`);
+    throw new Error(`Failed to fetch posts batch [limit=${limit}, lastId=${lastId}]: ${error}`);
   }
 }
 
